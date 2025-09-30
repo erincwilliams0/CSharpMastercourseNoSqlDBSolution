9497c8d6a9e454b077328be76830d70c38b657db
erin williams <erin.cwilliams0@gmail.com>
2025-09-30 11:34:27 -0500

    feat: tested all crud operations with mongoDb

--- MongoDBUI/Program.cs
+++ MongoDBUI/Program.cs
@@ -5,6 +5,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using System.Threading.Tasks.Dataflow;
 
 namespace MongoDBUI
 {
@@ -25,22 +26,77 @@ namespace MongoDBUI
 
             ContactModel user = new ContactModel
             {
-                FirstName = "Erin",
+                FirstName = "John",
                 LastName = "Williams"
             };
-            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "erin@mail.com" });
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "John@mail.com" });
             user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "me@mail.com" });
 
             user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-1212" });
             user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-1234" });
 
-            CreateContact(user);
+            //CreateContact(user);
+            //"d0e9af2f-4510-42f4-a6e2-b3085fb8ea8e"
 
 
+            //GetAllContacts();
+            //GetContactById("d0e9af2f-4510-42f4-a6e2-b3085fb8ea8e");
 
+            //7756212c-92be-47d6-aff6-07b6b39256f6
+
+            //UpdateContactsFirstName("John", "7756212c-92be-47d6-aff6-07b6b39256f6");
+
+            //RemovePhoneNumberFromUser("555-1212", "7756212c-92be-47d6-aff6-07b6b39256f6");
+
+            RemoveUser("d0e9af2f-4510-42f4-a6e2-b3085fb8ea8e");
+
+            GetAllContacts();
 
             Console.WriteLine("Done Processing MongoDb");
             Console.ReadLine();
+        }
+
+        public static void RemoveUser(string id)
+        {
+            Guid guid = Guid.Parse(id);
+            db.DeleteRecord<ContactModel>(tableName, guid);
+        }
+
+        public static void RemovePhoneNumberFromUser(string phoneNumber, string id)
+        {
+            Guid guid = Guid.Parse(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+
+            contact.PhoneNumbers = contact.PhoneNumbers.Where(x => x.PhoneNumber != phoneNumber).ToList();
+
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        private static void UpdateContactsFirstName(string firstName, string id)
+        {
+            Guid guid = Guid.Parse(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+
+            contact.FirstName = firstName;
+
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        private static void GetContactById(string id)
+        {
+            Guid guid = Guid.Parse(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+            Console.WriteLine($"{contact.Id}: {contact.FirstName} {contact.LastName}");
+        }
+
+        private static void GetAllContacts()
+        {
+            var contacts = db.LoadRecords<ContactModel>(tableName);
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"{contact.Id}: {contact.FirstName} {contact.LastName}");
+            }
         }
 
         private static void CreateContact(ContactModel contact)
