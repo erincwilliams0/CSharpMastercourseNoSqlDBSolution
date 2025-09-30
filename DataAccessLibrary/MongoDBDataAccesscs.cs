78b3f8983362d5d263876e0ece8f5bb5f600b2bb
erin williams <erin.cwilliams0@gmail.com>
2025-09-29 22:02:20 -0500

    Add project files.

---
+++ DataAccessLibrary/MongoDBDataAccesscs.cs
@@ -0,0 +1,61 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class MongoDBDataAccesscs
+    {
+        private IMongoDatabase db;
+        public MongoDBDataAccesscs(string dbName, string connectionString)
+        {
+            var client = new MongoClient(connectionString);
+            db = client.GetDatabase(dbName);
+
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+
+            return collection.Find(filter).First();
+        }
+
+        public void UpsertRecord<T>(string table, Guid id, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+
+
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", BsonValue.Create(id)),
+                record,
+                new ReplaceOptions { IsUpsert = true });
+        }
+
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
+
+        }
+    }
+}
