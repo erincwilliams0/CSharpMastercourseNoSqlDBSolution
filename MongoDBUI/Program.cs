9b5e3e5cbfdf189b33a15d9a00aeed94e0a34e43
erin williams <erin.cwilliams0@gmail.com>
2025-09-29 22:14:25 -0500

    feat: WIP

--- MongoDBUI/Program.cs
+++ MongoDBUI/Program.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace MongoDBUI
 {
     internal class Program
     {
+        private static MongoDBDataAccess db;
+        private static readonly string tableName = "Contacts";
+
         static void Main(string[] args)
         {
+            db = new MongoDBDataAccess("MongoContactsDb", GetConnectionString());
+
+
             Console.WriteLine("Done Processing MongoDb");
             Console.ReadLine();
         }
 
+        private static void CreateContact(ContactModel contact)
+        {
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
         private static string GetConnectionString(string connectionStringName = "Default")
         {
             string? output = "";
