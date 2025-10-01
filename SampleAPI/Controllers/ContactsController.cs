9888860530287c47eb329c708dca631d3b40ec4f
erin williams <erin.cwilliams0@gmail.com>
2025-10-01 15:08:50 -0500

    feat: WIP initial setup of Contacts controller with Get and Post api calls

--- SampleAPI/Controllers/ContactsController.cs
+++ SampleAPI/Controllers/ContactsController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 namespace SampleAPI.Controllers
 {
@@ -7,5 +13,31 @@ namespace SampleAPI.Controllers
     [ApiController]
     public class ContactsController : ControllerBase
     {
+        private IConfiguration _config;
+        private static MongoDBDataAccess db;
+        private static readonly string tableName = "Contacts";
+
+
+        public ContactsController(IConfiguration config)
+        {
+            //BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+            _config = config;
+            MongoClientSettings settings = MongoClientSettings.FromConnectionString(_config.GetConnectionString("Default"));
+
+
+            db = new MongoDBDataAccess("MongoContactsDb", settings);
+        }
+
+        [HttpGet]
+        public List<ContactModel> GetAll()
+        {
+            return db.LoadRecords<ContactModel>(tableName);
+        }
+
+        [HttpPost]
+        public void InsertRecord(ContactModel contact)
+        {
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
     }
 }
