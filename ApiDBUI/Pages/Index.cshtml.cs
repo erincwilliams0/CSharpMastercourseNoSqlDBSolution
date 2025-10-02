8c544fa0bc3e0365d1f0d02359bc1c8b45db7ae4
erin williams <erin.cwilliams0@gmail.com>
2025-10-01 21:19:58 -0500

    feat: tested out talking with api from front end razor pages application

--- ApiDBUI/Pages/Index.cshtml.cs
+++ ApiDBUI/Pages/Index.cshtml.cs
@@ -1,20 +1,73 @@
+using ApiDBUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ApiDBUI.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            await CreateContact();
+            await GetAllContacts();
+        }
 
+
+        private async Task CreateContact()
+        {
+            ContactModel contact = new ContactModel
+            {
+                FirstName = "Erin",
+                LastName = "Williams"
+            };
+            contact.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "Erin@mail.com" });
+            contact.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "me@mail.com" });
+
+            contact.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-1212" });
+            contact.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-1234" });
+
+            var _client = _httpClientFactory.CreateClient();
+            var response = await _client.PostAsync(
+                "https://localhost:44371/api/Contacts",
+                new StringContent(
+                    JsonSerializer.Serialize(contact),
+                    Encoding.UTF8,
+                    "application/json"));
+        }
+
+        private async Task GetAllContacts()
+        {
+            var _client = _httpClientFactory.CreateClient();
+            var response = await _client.GetAsync("https://localhost:44371/api/Contacts");
+
+            List<ContactModel> contacts;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                string responseText = await response.Content.ReadAsStringAsync();
+                contacts = JsonSerializer.Deserialize<List<ContactModel>>(responseText, options);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
         }
     }
 }
